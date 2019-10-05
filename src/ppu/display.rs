c3fcace503845e585bcd4f3cafbfe4f1f4e1825b
niketin <Niketin@users.noreply.github.com>
2019-10-05 13:09:43 +0300

    Modify display to use Vec<u8> internally.

--- src/ppu/display.rs
+++ src/ppu/display.rs
@@ -1,9 +1,9 @@
 
 
 pub struct Display {
-    width: usize,
-    height: usize,
-    array: Vec<Color>
+    pub width: usize,
+    pub height: usize,
+    array: Vec<u8>
 }
 
 impl Display {
@@ -11,20 +11,23 @@ impl Display {
         Display {
             width,
             height,
-            array: vec![Color::new(); width * height]
+            array: vec![0u8; width * height * 3]
         }
     }
+    
 
-    pub fn get_pixel(&self, x: usize, y: usize) -> &Color {
-        &self.array[self.width * y + x]
+    pub fn get_pixel(&self, x: usize, y: usize) -> &[u8] {
+        &self.array[(self.width * y + x) * 3 .. (self.width * y + x) * 3 + 2]
     }
 
-    pub fn get_pixels(&self) -> &Vec<Color> {
-        &self.array
+    pub fn get_pixels(&self) -> &[u8] {
+        &self.array[..]
     }
 
     pub fn set_pixel(&mut self, x: usize, y: usize, color: Color) {
-        self.array[self.width * y + x] = color;
+        self.array[(self.width * y + x) * 3 ] = color.r;
+        self.array[(self.width * y + x) * 3 + 1] = color.g;
+        self.array[(self.width * y + x) * 3 + 2] = color.b;
     }
 }
 
@@ -42,11 +45,11 @@ pub struct Color {
 }
 
 impl Color {
-    fn new() -> Color {
+    pub fn new() -> Color {
         Color { r: 0, g: 0, b: 0}
     }
 
-    fn new_rgb(r: u8, g: u8, b: u8) -> Color {
+    pub fn new_rgb(r: u8, g: u8, b: u8) -> Color {
         Color { r, g, b }
     }
 }
