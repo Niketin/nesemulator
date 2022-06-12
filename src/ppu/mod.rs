70ecfd84bf77cddb19e254acb62a2ac245738b3c
Niketin <Niketin@users.noreply.github.com>
2022-06-12 15:16:37 +0300

    Refactor PPU to support scrolling related registers

--- src/ppu/mod.rs
+++ src/ppu/mod.rs
@@ -18,7 +18,7 @@ const MASK_FLIP_SPRITE_VERTICALLY: u8 = 0b1000_0000;
 
 
 pub struct Ppu {
-    pub ppuaddr: u16,
+    pub n_v: u16, // Current VRAM address (15 bits)
     pub ppuctrl: u8,
     pub ppudata: u8,
     pub ppumask: u8,
@@ -34,8 +34,6 @@ pub struct Ppu {
     palette: Palette,
     pub nmi_occurred: bool,
     pub nmi_output: bool,
-    pub ppuaddr_upper_byte_next: bool,
-    ppuaddr_upper_byte: u8,
     ppudata_buffer: u8,
     shift_att_table: ShiftRegister,
     shift_pattern_l: ShiftRegister,
@@ -59,12 +57,15 @@ pub struct Ppu {
     oam_counters: [u8; 8],
     oam_sprite_fetched_y: u8,
     oam_sprite_fetched_tile_index: u8,
+    n_t: u16, // Temporary VRAM address (15 bits); can also be thought of as the address of the top left onscreen tile.
+    n_x: u16, // Fine X scroll (3 bits)
+    n_w: bool, // First or second write toggle (1 bit)
 }
 
 impl Ppu {
     pub fn new(bus: Bus) -> Ppu {
         Ppu {
-            ppuaddr: 0,
+            n_v: 0,
             ppuctrl: 0,
             ppudata: 0,
             ppumask: 0,
@@ -80,8 +81,6 @@ impl Ppu {
             palette: Palette::new(),
             nmi_occurred: false,
             nmi_output: true,
-            ppuaddr_upper_byte_next: true,
-            ppuaddr_upper_byte: 0,
             ppudata_buffer: 0,
             shift_att_table: ShiftRegister::new(2),
             shift_pattern_l: ShiftRegister::new(2),
@@ -105,24 +104,108 @@ impl Ppu {
             oam_counters: [0; 8],
             oam_sprite_fetched_y: 0,
             oam_sprite_fetched_tile_index: 0,
+            n_t: 0,
+            n_x: 0,
+            n_w: false,
         }
     }
 
-    pub fn write_ppuaddr(&mut self, value: u8) {
-        if self.ppuaddr_upper_byte_next {
-            self.ppuaddr_upper_byte = value;
-            self.ppuaddr_upper_byte_next = false;
-        } else {
-            self.ppuaddr = (self.ppuaddr_upper_byte as u16) << 8 | (value as u16);
-            self.ppuaddr_upper_byte_next = true;
+    fn get_coarse_x_scroll(&self) -> u16 {
+        return self.n_v & 0x001f
+    }
+
+    fn get_coarse_y_scroll(&self) -> u16 {
+        return (self.n_v >> 5) & 0x001f
+    }
+
+    fn _get_nametable_select(&self) -> u16 {
+        return (self.n_v >> 10) & 0x0003
+    }
+
+    fn get_fine_y_scroll(&self) -> u16 {
+        return (self.n_v >> 12) & 0x0007
+    }
+
+    fn coarse_x_increment(&mut self) {
+        if self.get_coarse_x_scroll() == 0x001f {
+            self.n_v &= !0x001f;
+            self.n_v ^= 0x0400;
         }
+        else {
+            self.n_v += 1;
+        }
+    }
+
+    fn y_increment(&mut self) {
+        if self.get_fine_y_scroll() < 7 {
+            self.n_v += 0x1000;
+        }
+        else {
+            self.n_v &= !0x7000;
+            let mut y = self.get_coarse_y_scroll();
+            if y == 29 {
+                y = 0;
+                self.n_v ^= 0x0800;
+            }
+            else if y == 31 {
+                y = 0;
+            }
+            else {
+                y += 1;
+            }
+            self.n_v = (self.n_v & !0x03e0) | (y << 5);
+        }
+    }
+
+    /// Returns the current tile address.
+    fn get_tile_address(&self) -> u16 {
+        0x2000 | (self.n_v & 0x0fff)
+    }
+
+    /// Returns the current attribute address.
+    /// NN 1111 YYY XXX
+    /// || |||| ||| +++-- high 3 bits of coarse X (x/4)
+    /// || |||| +++------ high 3 bits of coarse Y (y/4)
+    /// || ++++---------- attribute offset (960 bytes)
+    /// ++--------------- nametable select
+    fn get_attribute_address(&self) -> u16 {
+        0x23c0 | (self.n_v & 0x0c00) | ((self.n_v >> 4) & 0x38) | ((self.n_v >> 2) & 0x07)
+    }
+
+    fn rendering_enabled(&self) -> bool {
+        (self.ppumask & 0x18) > 0
+    }
+
+    pub fn write_ppuscroll(&mut self, value: u8) {
+        if !self.n_w {
+            self.n_t &= 0xffe0;
+            self.n_t |= value as u16 >> 3;
+            self.n_x = value as u16 & 0x07;
+        } else {
+            self.n_t &= 0b00001100_00011111;
+            self.n_t |= ((value as u16) & 0xf8) << 2;
+            self.n_t |= ((value as u16) & 0x07) << 12;
+        }
+        self.n_w = !self.n_w;
+    }
+
+    pub fn write_ppuaddr(&mut self, value: u8) {
+        if !self.n_w {
+            self.n_t &= 0x00ff;
+            self.n_t |= ((value & 0x3f) as u16) << 8;
+        } else {
+            self.n_t &= 0xff00;
+            self.n_t |= value as u16;
+            self.n_v = self.n_t;
+        }
+        self.n_w = !self.n_w;
     }
 
     pub fn read_ppustatus(&mut self) -> u8 {
         let status = self.ppustatus;
         self.clear_vblank();
         self.nmi_occurred = false; // and this variable are kinda the same.
-        self.ppuaddr_upper_byte_next = true;
+        self.n_w = false;
         status
     }
 
@@ -146,26 +229,28 @@ impl Ppu {
     /// [NESDEV]: https://wiki.nesdev.com/w/index.php/PPU_registers#:~:text=scrolling.-,The%20PPUDATA%20read%20buffer%20(post-fetch),-When
     pub fn read_ppudata(&mut self) -> u8 {
         let mut result = self.ppudata_buffer;
-        self.ppudata_buffer = self.bus.read(self.ppuaddr);
-        if (0x3F00..=0x3FFF).contains(&self.ppuaddr) {
+        self.ppudata_buffer = self.bus.read(self.n_v);
+        if (0x3F00..=0x3FFF).contains(&self.n_v) {
             // Update the result with the newly updated buffer value that contains value from palette.
             result = self.ppudata_buffer;
             // Update the buffer with a value from VRAM address-0x1000
-            self.ppudata_buffer = self.bus.read(self.ppuaddr - 0x1000);
+            self.ppudata_buffer = self.bus.read(self.n_v - 0x1000);
         }
-        self.ppuaddr += self.get_vram_address_increment();
+        self.n_v += self.get_vram_address_increment();
         result
     }
 
     /// Writes a value to VRAM
     pub fn write_ppudata(&mut self, value: u8) {
-        self.bus.write(self.ppuaddr, value);
-        self.ppuaddr += self.get_vram_address_increment();
+        self.bus.write(self.n_v, value);
+        self.n_v += self.get_vram_address_increment();
     }
 
     pub fn write_ppuctrl(&mut self, value: u8) {
         self.nmi_output = (value & 0x80) == 0x80;
         self.ppuctrl = value;
+        self.n_t &= !0xc0;
+        self.n_t |= ((value & 0x03) as u16) << 10;
     }
 
     fn get_vram_address_increment(&self) -> u16 {
@@ -247,6 +332,11 @@ impl Ppu {
     }
 
     fn fetch_match(&mut self) {
+        //TODO is this needed?
+        if !self.rendering_enabled() {
+            return;
+        }
+
         match ((self.x - 1) % 8) + 1 {
             1 => (),
             2 => self.fetch_nametable_byte(),
@@ -292,14 +382,52 @@ impl Ppu {
                 self.shift_patterns();
                 self.fetch_match();
             },
-            257 => (),
-            258..=320 => (),
+            257 => {
+                self.oamaddr = 0; // "Set to 0 during each of ticks 257-320 (the sprite tile loading interval) of the pre-render and visible scanlines" https://www.nesdev.org/wiki/PPU_registers#OAMADDR
+            },
+            258..=320 => {
+                self.oamaddr = 0; // "Set to 0 during each of ticks 257-320 (the sprite tile loading interval) of the pre-render and visible scanlines" https://www.nesdev.org/wiki/PPU_registers#OAMADDR
+            },
             321..=336 => {
                 self.shift_patterns();
                 self.fetch_match();
             },
             337..=340 => (),
             _ => unreachable!("PPU cycle count {} exceeds 340", self.y),
+        }
+
+        if self.rendering_enabled() {
+            self.update_xy();
+        }
+    }
+
+    fn update_xy(&mut self) {
+
+        if self.x == 256 {
+            // Inc. vert(v)
+            self.y_increment();
+            self.coarse_x_increment();
+            return;
+        }
+
+        if self.x == 257 {
+            // hori(v) = hori(t)
+            self.n_v &= !0x041f;
+            self.n_v |= self.n_t & 0x041f;
+            return;
+        }
+
+        if self.y == 261 && (280..=304).contains(&self.x) {
+            // vert(v) = vert(t)
+            self.n_v &= !0x7be0;
+            self.n_v |= self.n_t & 0x7be0;
+            return;
+        }
+
+        if self.x % 8 == 0 && ((8..=248).contains(&self.x) || (328..=336).contains(&self.x)) {
+            // Inc. hori(v)
+            self.coarse_x_increment();
+            return;
         }
     }
 
@@ -475,7 +603,7 @@ impl Ppu {
                 if pattern == 0 {
                     continue;
                 }
-                
+
                 let attribute = self.oam_latches[i];
                 let palette_number = (attribute & 0x3) + 4;
                 let color_address = (palette_number << 2) + pattern;
@@ -488,7 +616,6 @@ impl Ppu {
             }
         }
 
-        
         if 1 <= self.x && self.x <= 256 {
             let show_background = (self.ppumask >> 3) & 1 == 1;
 
@@ -516,7 +643,7 @@ impl Ppu {
                     self.oam_pattern_low[i] >>= 1;
                 }
             }
-    
+
             // Decrease oam counters
             self.oam_counters
                 .iter_mut()
@@ -526,6 +653,7 @@ impl Ppu {
     }
 
     fn get_background_color(&self) -> display::Color {
+        //TODO Apply scrolling somehow
         debug_assert!(1<= self.x && self.x <= 256);
         let pattern_l = self.shift_pattern_l.get() & 1;
         let pattern_h = self.shift_pattern_h.get() & 1;
@@ -540,46 +668,15 @@ impl Ppu {
     }
 
     fn fetch_nametable_byte(&mut self) {
-        let nametable_address = self.nametable_address();
-        let (x, y) = self.get_next_tile_xy();
-        let cell_x = x >> 3;
-        let cell_y = y >> 3;
-        let address = nametable_address + cell_x + (cell_y << 5);
+        let address = self.get_tile_address();
         let nt_entry = self.bus.read(address);
         self.latch_nametable = nt_entry;
     }
 
     fn fetch_attribute_table_byte(&mut self) {
-        let nametable_address = self.nametable_address();
-        let (x, y) = self.get_next_tile_xy();
-        let cell_x = x >> 5;
-        let cell_y = y >> 5;
-        let address = nametable_address + 0x03C0 + (cell_y << 3) + cell_x;
+        let address = self.get_attribute_address();
         let att_entry = self.bus.read(address);
         self.latch_attribute = att_entry;
-    }
-
-    fn nametable_address(&self) -> u16 {
-        let nametable_number = self.ppuctrl & 0x03;
-        0x2000 + ((nametable_number as u16) << 10)
-    }
-
-    fn get_next_tile_xy(&self) -> (u16, u16) {
-        // x has to be at the tile after the next tile
-        let x = if 321 <= self.x && self.x <= 336 {
-            self.x-321
-        }
-        else {
-            self.mod_x(self.x + 16)
-        };
-
-        // y has to be at the tile after the next tile
-        let y = if x < self.x {
-            self.next_y()
-        } else {
-            self.y
-        };
-        (x, y)
     }
 
     /// Returns the address of the given tile in the given pattern table.
@@ -623,8 +720,7 @@ impl Ppu {
         let bg_pattern_tile_address = self.get_pattern_table_tile_address(
             bg_pattern_table_address,
             self.latch_nametable);
-        let (_, y) = self.get_next_tile_xy();
-        let pattern_fine_y_offset = y & 0b0111;
+        let pattern_fine_y_offset = self.get_fine_y_scroll();
         bg_pattern_tile_address | pattern_fine_y_offset
     }
 
